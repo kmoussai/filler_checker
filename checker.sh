d7f6a5be22f8de752d56b3244e8c864e3f1b2df6
Khalid Moussaid <kmoussai@e1r9p7.1337.ma>
2019-10-07 18:51:17 +0100

    add checker args

--- checker.sh
+++ checker.sh
@@ -3,12 +3,13 @@ GREEN='\033[0;32m'
 RESET='\033[0m'
 RED='\033[0;31m'
 #path of your player
-MYPLAYER="./players/khalid.filler"
+MYPLAYER=$1 #"./players/khalid.filler"
 MYPLAYER_NUMBER=2
 PLAYERS="carli.filler abanlin.filler champely.filler grati.filler hcao.filler superjeannot.filler"
 PLAYER_NUMBER=1
 #Map here <map00 map01 map02>
-MAP="map01"
+MAP=$2
+#exit
 COUNT=5
 Total=0
 batle=0
@@ -18,12 +19,12 @@ do
     win=0
     echo $PLAYER" VS "$MYPLAYER" In $MAP"
     # all battle historique is in trace folder
-    MPATH=trace/$PLAYER/$MAP
+   	MPATH=trace/$PLAYER/$MAP
     nothing=`mkdir -p $MPATH/`
     for i in `seq 1 $COUNT`
     do
         ./filler_vm -p$MYPLAYER_NUMBER $MYPLAYER -f maps/$MAP -p$PLAYER_NUMBER players/$PLAYER > $MPATH/$i 
-        res=`tail -2 $MPATH/$i | cut -f4 -d ' '`
+		res=`tail -2 $MPATH/$i | cut -f4 -d ' '`
         x_score=`echo $res | cut -f2 -d ' '`
         o_score=`echo $res | cut -f1 -d ' '`
         if [ $MYPLAYER_NUMBER -eq 2 ]
